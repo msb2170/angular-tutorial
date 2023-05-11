f0d3c684268678df1c6b1df3c3fb06e736989163
Matthew <msb2170@gmail.com>
2023-05-10 21:12:34 -0400

    style changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,4 +32,11 @@ export class AppComponent {
     }
     return this.allItems.filter((item) => this.filter === "done" ? item.done : !item.done)
   }
+
+  addItem(description: string) {
+    this.allItems.unshift({
+      description,
+      done: false
+    })
+  }
 }
